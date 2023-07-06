4041e9e609dfaf4c06918ec21bc38e66c0d0eb4b
MiVergar <75232401+MiVergar@users.noreply.github.com>
2023-07-05 22:23:15 -0400

    Update MainTest.java

--- Tarea3PdS/src/test/java/paquete/MainTest.java
+++ Tarea3PdS/src/test/java/paquete/MainTest.java
@@ -5,49 +5,76 @@ import static org.junit.Assert.*;
 public class MainTest {
     @Test
     public void testBuyGameAdmin() {
-        // Arrange
+    	
         Main.inventory.clear();
+        Main.earnings = 0;
         Game game = new Game("Test Game", 50000, "Test Genre", "Test Platform", 100);
         Main.AddGame(game);
 
-        // Act
         boolean result = Main.BuyGameAdmin("Test Game", 50);
 
-        // Assert
         assertTrue(result);
         assertEquals(150, game.amount);
         assertEquals(-1250000, Main.earnings);
     }
 
     @Test
-    public void testBuyGameAdmin_NonExistentGame() {
-        // Arrange
+    public void testBuyGameAdminNonExistentGame() {
+
         Main.inventory.clear();
 
-        // Act
         boolean result = Main.BuyGameAdmin("Nonexistent Game", 50);
 
-        // Assert
         assertFalse(result);
     }
 
-    // Agrega más métodos de prueba para cubrir otros escenarios
-
     @Test
     public void testSellGame() {
-        // Arrange
+
         Main.inventory.clear();
+        Main.earnings = 0;
         Game game = new Game("Test Game", 50000, "Test Genre", "Test Platform", 100);
         Main.AddGame(game);
 
-        // Act
         boolean result = Main.SellGame("Test Game", 50);
 
-        // Assert
+        assertTrue(result);
+        assertEquals(50, game.amount);
+        assertEquals(2500000, Main.earnings);
+    }
+    
+    @Test
+    public void testSellGameNonExistentGame() {
+
+        Main.inventory.clear();
+
+        boolean result = Main.SellGame("Nonexistent Game", 50);
+
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testBuyGameClient() {
+    	
+        Main.inventory.clear();
+        Main.earnings = 0;
+        Game game = new Game("Test Game", 50000, "Test Genre", "Test Platform", 100);
+        Main.AddGame(game);
+
+        boolean result = Main.BuyGameClient("Test Game", 50);
+
         assertTrue(result);
         assertEquals(50, game.amount);
         assertEquals(2500000, Main.earnings);
     }
 
-    // Agrega más métodos de prueba para cubrir otros escenarios
+    @Test
+    public void testBuyGameClientNonExistentGame() {
+
+        Main.inventory.clear();
+
+        boolean result = Main.BuyGameAdmin("Nonexistent Game", 50);
+
+        assertFalse(result);
+    }
 }
